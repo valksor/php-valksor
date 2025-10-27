9a0a6382ff609de44ffe40b077a072775c996eca
k0d3r1s <k0d3r1s@gmail.com>
2025-10-27 15:54:46 +0200

    do not use utf symbols in php code comments

--- src/Valksor/Functions/Php/Traits/_SetStatic.php
+++ src/Valksor/Functions/Php/Traits/_SetStatic.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Valksor package.
  *
- * (c) Dāvis Zālītis (k0d3r1s)
+ * (c) Davis Zalitis (k0d3r1s)
  * (c) SIA Valksor <packages@valksor.com>
  *
  * For the full copyright and license information, please view the LICENSE
