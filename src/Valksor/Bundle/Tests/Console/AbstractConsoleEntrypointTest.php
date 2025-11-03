6f7044013e106c6720f21964876efa6012e0b844
k0d3r1s <k0d3r1s@gmail.com>
2025-11-03 03:53:25 +0200

    add some bundle tests

--- src/Valksor/Bundle/Tests/Console/AbstractConsoleEntrypointTest.php
+++ src/Valksor/Bundle/Tests/Console/AbstractConsoleEntrypointTest.php
@@ -14,23 +14,169 @@ namespace Valksor\Bundle\Tests\Console;
 
 use LogicException;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Console\Exception\ExceptionInterface;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Valksor\Bundle\Console\AbstractConsoleEntrypoint;
 use Valksor\Bundle\Console\MultiAppApplication;
 
+use function is_dir;
 use function mkdir;
+use function scandir;
 use function sys_get_temp_dir;
 use function tempnam;
+use function touch;
 use function unlink;
 
 final class AbstractConsoleEntrypointTest extends TestCase
 {
     private string $projectDir;
 
+    public function testCreateReturnsMultiAppApplicationWhenNoIdProvided(): void
+    {
+        FunctionalConsoleEntrypoint::resetState();
+        FunctionalMultiAppApplication::resetState();
+        FunctionalConsoleEntrypoint::$projectDir = $this->projectDir;
+
+        $factory = FunctionalConsoleEntrypoint::create($this->projectDir);
+
+        $input = new ArrayInput([
+            'command' => 'cache:clear',
+        ]);
+        $context = [
+            'APP_ENV' => 'prod',
+            'APP_DEBUG' => '0',
+        ];
+
+        $application = $factory($input, $context);
+
+        self::assertInstanceOf(FunctionalMultiAppApplication::class, $application);
+        self::assertNotEmpty(FunctionalConsoleEntrypoint::$multiAppCalls);
+        self::assertSame([
+            ['prod', false, 'alpha.main'],
+        ], FunctionalMultiAppApplication::$kernelCalls);
+    }
+
+    public function testCreateReturnsSingleAppApplication(): void
+    {
+        FunctionalConsoleEntrypoint::resetState();
+        FunctionalMultiAppApplication::resetState();
+        FunctionalConsoleEntrypoint::$projectDir = $this->projectDir;
+
+        $factory = FunctionalConsoleEntrypoint::create($this->projectDir);
+
+        $input = new ArrayInput([
+            'command' => 'cache:clear',
+            '--id' => 'alpha',
+        ]);
+        $context = [
+            'APP_ENV' => 'dev',
+            'APP_DEBUG' => '1',
+        ];
+
+        $application = $factory($input, $context);
+
+        self::assertInstanceOf(Application::class, $application);
+        self::assertSame([
+            ['prod', false, 'alpha.main'],
+        ], FunctionalConsoleEntrypoint::$kernelCalls);
+        self::assertSame([], FunctionalConsoleEntrypoint::$multiAppCalls);
+    }
+
+    public function testCreateThrowsWhenConfigurationMissing(): void
+    {
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Apps directory not configured. Set $appsDir property in ' . MisconfiguredConsoleEntrypoint::class);
+
+        MisconfiguredConsoleEntrypoint::create($this->projectDir);
+    }
+
+    public function testCreateThrowsWhenDependenciesMissing(): void
+    {
+        $this->removeDirectory($this->vendorPath());
+
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Dependencies are missing. Try running "composer install".');
+
+        FunctionalConsoleEntrypoint::resetState();
+        FunctionalConsoleEntrypoint::create($this->projectDir);
+    }
+
+    public function testCreateThrowsWhenNoApplicationsFound(): void
+    {
+        FunctionalConsoleEntrypoint::resetState();
+        FunctionalMultiAppApplication::resetState();
+
+        $this->removeDirectory($this->projectDir . '/apps/alpha.main');
+        $this->removeDirectory($this->projectDir . '/apps/beta.stage');
+
+        if (!is_dir($this->projectDir . '/apps')) {
+            mkdir($this->projectDir . '/apps');
+        }
+
+        $factory = FunctionalConsoleEntrypoint::create($this->projectDir);
+
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('No applications found in appDir');
+
+        $factory(new ArrayInput(['command' => 'cache:clear']), [
+            'APP_ENV' => 'prod',
+            'APP_DEBUG' => '0',
+        ]);
+    }
+
+    public function testCreateThrowsWhenRuntimeMissing(): void
+    {
+        unlink($this->vendorPath() . '/autoload_runtime.php');
+
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Symfony Runtime is missing. Try running "composer require symfony/runtime".');
+
+        FunctionalConsoleEntrypoint::resetState();
+        FunctionalConsoleEntrypoint::create($this->projectDir);
+    }
+
     public function testDiscoverAppsReturnsSortedDirectories(): void
     {
         $apps = TestConsoleEntrypoint::exposeDiscoverApps($this->projectDir);
         self::assertSame(['alpha.main', 'beta.stage'], $apps);
+    }
+
+    /**
+     * @throws ExceptionInterface
+     */
+    public function testMultiAppApplicationHandlesListCommand(): void
+    {
+        FunctionalMultiAppApplication::resetState();
+        FunctionalMultiAppApplication::$projectDir = $this->projectDir;
+
+        $application = new FunctionalMultiAppApplication(
+            ['alpha.main', 'beta.stage'],
+            [
+                'APP_ENV' => 'dev',
+                'APP_DEBUG' => '1',
+            ],
+            $this->projectDir,
+            'apps',
+            'infra',
+        );
+
+        $output = new BufferedOutput();
+        $exitCode = $application->doRun(new ArrayInput(['command' => 'list']), $output);
+
+        self::assertSame(0, $exitCode);
+        self::assertStringContainsString('Multi-App Console', $output->fetch());
+        self::assertSame([
+            ['prod', false, 'alpha.main'],
+        ], FunctionalMultiAppApplication::$kernelCalls);
     }
 
     public function testResolveAppAliasMatchesUniqueAlias(): void
@@ -49,10 +195,16 @@ final class AbstractConsoleEntrypointTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->projectDir = tempnam(sys_get_temp_dir(), 'valksor_apps_');
+        $this->projectDir = tempnam(sys_get_temp_dir(), 'valksor_bundle_');
         unlink($this->projectDir);
+        mkdir($this->projectDir);
+
+        mkdir($this->projectDir . '/apps', 0o777, true);
         mkdir($this->projectDir . '/apps/alpha.main', 0o777, true);
         mkdir($this->projectDir . '/apps/beta.stage', 0o777, true);
+        mkdir($this->projectDir . '/infra', 0o777, true);
+        mkdir($this->projectDir . '/vendor', 0o777, true);
+        touch($this->projectDir . '/vendor/autoload_runtime.php');
     }
 
     protected function tearDown(): void
@@ -82,6 +234,11 @@ final class AbstractConsoleEntrypointTest extends TestCase
         }
 
         rmdir($directory);
+    }
+
+    private function vendorPath(): string
+    {
+        return $this->projectDir . '/vendor';
     }
 }
 
@@ -122,5 +279,223 @@ final class TestConsoleEntrypoint extends AbstractConsoleEntrypoint
         ?string $infrastructureDir = null,
     ): MultiAppApplication {
         throw new LogicException('Multi-app application creation is not expected during this test.');
+    }
+}
+
+/**
+ * @internal
+ */
+final class MisconfiguredConsoleEntrypoint extends AbstractConsoleEntrypoint
+{
+    protected static ?string $appsDir = null;
+    protected static ?string $infrastructureDir = 'infra';
+
+    protected static function createKernel(
+        string $environment,
+        bool $debug,
+        string $appId,
+    ): KernelInterface {
+        throw new LogicException('Not required for this test.');
+    }
+
+    protected static function createMultiAppApplication(
+        array $availableApps,
+        array $context,
+        string $projectDir,
+        ?string $appsDir = null,
+        ?string $infrastructureDir = null,
+    ): MultiAppApplication {
+        throw new LogicException('Not required for this test.');
+    }
+}
+
+/**
+ * @internal
+ */
+final class FunctionalConsoleEntrypoint extends AbstractConsoleEntrypoint
+{
+    /** @var array<int, array{0: string, 1: bool, 2: string}> */
+    public static array $kernelCalls = [];
+
+    /** @var array<int, array{0: array<int, string>, 1: array<string, mixed>, 2: string, 3: ?string, 4: ?string}> */
+    public static array $multiAppCalls = [];
+
+    public static ?string $projectDir = null;
+    protected static ?string $appsDir = 'apps';
+    protected static ?string $infrastructureDir = 'infra';
+
+    public static function resetState(): void
+    {
+        self::$projectDir = null;
+        self::$kernelCalls = [];
+        self::$multiAppCalls = [];
+    }
+
+    protected static function createKernel(
+        string $environment,
+        bool $debug,
+        string $appId,
+    ): KernelInterface {
+        self::$kernelCalls[] = [$environment, $debug, $appId];
+
+        return new DummyKernel($environment, $debug, $appId, self::$projectDir ?? '');
+    }
+
+    protected static function createMultiAppApplication(
+        array $availableApps,
+        array $context,
+        string $projectDir,
+        ?string $appsDir = null,
+        ?string $infrastructureDir = null,
+    ): MultiAppApplication {
+        self::$multiAppCalls[] = [$availableApps, $context, $projectDir, $appsDir, $infrastructureDir];
+        self::$projectDir = $projectDir;
+        FunctionalMultiAppApplication::$projectDir = $projectDir;
+
+        return new FunctionalMultiAppApplication($availableApps, $context, $projectDir, $appsDir, $infrastructureDir);
+    }
+}
+
+/**
+ * @internal
+ */
+final class FunctionalMultiAppApplication extends MultiAppApplication
+{
+    /** @var array<int, array{0: string, 1: bool, 2: string}> */
+    public static array $kernelCalls = [];
+    public static ?string $projectDir = null;
+
+    public static function resetState(): void
+    {
+        self::$projectDir = null;
+        self::$kernelCalls = [];
+    }
+
+    protected static function createKernel(
+        string $environment,
+        bool $debug,
+        string $appId,
+    ): KernelInterface {
+        self::$kernelCalls[] = [$environment, $debug, $appId];
+
+        return new DummyKernel($environment, $debug, $appId, self::$projectDir ?? '');
+    }
+}
+
+/**
+ * @internal
+ */
+final class DummyKernel implements KernelInterface
+{
+    private bool $booted = false;
+    private ContainerBuilder $container;
+    private float $startTime;
+
+    public function __construct(
+        private readonly string $environment,
+        private readonly bool $debug,
+        private readonly string $appId,
+        private readonly string $projectDir,
+    ) {
+        $this->startTime = microtime(true);
+        $this->container = new ContainerBuilder();
+        $this->container->set('event_dispatcher', new EventDispatcher());
+        $this->container->setParameter('console.command.ids', []);
+        $this->container->setParameter('console.lazy_command.ids', []);
+    }
+
+    public function boot(): void
+    {
+        $this->booted = true;
+    }
+
+    public function getBuildDir(): string
+    {
+        return $this->getProjectDir() . '/var/build/' . $this->environment;
+    }
+
+    public function getBundle(
+        string $name,
+    ): BundleInterface {
+        throw new LogicException('No bundles registered.');
+    }
+
+    public function getBundles(): array
+    {
+        return [];
+    }
+
+    public function getCacheDir(): string
+    {
+        return $this->getProjectDir() . '/var/cache/' . $this->environment;
+    }
+
+    public function getCharset(): string
+    {
+        return 'UTF-8';
+    }
+
+    public function getContainer(): ContainerBuilder
+    {
+        return $this->container;
+    }
+
+    public function getEnvironment(): string
+    {
+        return $this->environment;
+    }
+
+    public function getLogDir(): string
+    {
+        return $this->getProjectDir() . '/var/log';
+    }
+
+    public function getProjectDir(): string
+    {
+        return '' !== $this->projectDir ? $this->projectDir : sys_get_temp_dir();
+    }
+
+    public function getShareDir(): string
+    {
+        return $this->getProjectDir() . '/var/share';
+    }
+
+    public function getStartTime(): float
+    {
+        return $this->startTime;
+    }
+
+    public function handle(
+        Request $request,
+        int $type = self::MAIN_REQUEST,
+        bool $catch = true,
+    ): Response {
+        return new Response('OK');
+    }
+
+    public function isDebug(): bool
+    {
+        return $this->debug;
+    }
+
+    public function locateResource(
+        string $name,
+    ): string {
+        return $name;
+    }
+
+    public function registerBundles(): iterable
+    {
+        return [];
+    }
+
+    public function registerContainerConfiguration(
+        LoaderInterface $loader,
+    ): void {
+    }
+
+    public function shutdown(): void
+    {
+        $this->booted = false;
     }
 }
--- src/Valksor/Bundle/Tests/Kernel/AbstractKernelTest.php
+++ src/Valksor/Bundle/Tests/Kernel/AbstractKernelTest.php
@@ -15,6 +15,14 @@ namespace Valksor\Bundle\Tests\Kernel;
 use PHPUnit\Framework\TestCase;
 use ReflectionException;
 use ReflectionMethod;
+use Symfony\Component\Config\FileLocatorInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader as DiPhpFileLoader;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Symfony\Component\Routing\Loader\PhpFileLoader as RoutingPhpFileLoader;
+use Symfony\Component\Routing\RouteCollection;
 use Valksor\Bundle\Kernel\AbstractKernel;
 
 use function file_put_contents;
@@ -22,6 +30,7 @@ use function is_dir;
 use function is_file;
 use function mkdir;
 use function scandir;
+use function sort;
 use function sys_get_temp_dir;
 use function tempnam;
 use function unlink;
@@ -33,6 +42,76 @@ if (!class_exists('Symfony\\Component\\Dotenv\\Dotenv')) {
 final class AbstractKernelTest extends TestCase
 {
     private string $projectDir;
+
+    public function testConfigureContainerImportsInfrastructureAndAppConfigs(): void
+    {
+        mkdir($this->projectDir . '/infra/config/packages/dev', 0o777, true);
+        mkdir($this->projectDir . '/apps/app.main/config/packages/dev', 0o777, true);
+
+        file_put_contents($this->projectDir . '/infra/config/packages/foo.php', '<?php');
+        file_put_contents($this->projectDir . '/infra/config/packages/bar.php', '<?php');
+        file_put_contents($this->projectDir . '/apps/app.main/config/packages/bar.override.php', '<?php');
+        file_put_contents($this->projectDir . '/infra/config/packages/dev/env.php', '<?php');
+        file_put_contents($this->projectDir . '/infra/config/packages/dev/skip.php', '<?php');
+        file_put_contents($this->projectDir . '/apps/app.main/config/packages/dev/skip.override.php', '<?php');
+        file_put_contents($this->projectDir . '/infra/config/services.php', '<?php');
+        file_put_contents($this->projectDir . '/apps/app.main/config/services.php', '<?php');
+
+        $kernel = new TestKernel($this->projectDir, 'app.main', 'dev');
+
+        $imports = [];
+        $configurator = $this->createContainerConfigurator($imports);
+        $kernel->exposeConfigureContainer($configurator);
+
+        $expected = [
+            $this->projectDir . '/infra/config/packages/foo.php',
+            $this->projectDir . '/infra/config/packages/dev/env.php',
+            $this->projectDir . '/infra/config/services.php',
+            $this->projectDir . '/apps/app.main/config/{packages}/*.php',
+            $this->projectDir . '/apps/app.main/config/{packages}/dev/*.php',
+            $this->projectDir . '/apps/app.main/config/services.php',
+        ];
+
+        sort($imports);
+        sort($expected);
+
+        self::assertSame($expected, $imports);
+    }
+
+    public function testConfigureRoutesRespectsOverrides(): void
+    {
+        mkdir($this->projectDir . '/infra/config/routes/dev', 0o777, true);
+        mkdir($this->projectDir . '/apps/app.main/config/routes/dev', 0o777, true);
+
+        file_put_contents($this->projectDir . '/infra/config/routes/alpha.php', '<?php');
+        file_put_contents($this->projectDir . '/infra/config/routes/beta.php', '<?php');
+        file_put_contents($this->projectDir . '/apps/app.main/config/routes/beta.override.php', '<?php');
+        file_put_contents($this->projectDir . '/infra/config/routes/dev/gamma.php', '<?php');
+        file_put_contents($this->projectDir . '/infra/config/routes/dev/delta.php', '<?php');
+        file_put_contents($this->projectDir . '/apps/app.main/config/routes/dev/delta.override.php', '<?php');
+        file_put_contents($this->projectDir . '/infra/config/routes.php', '<?php');
+        file_put_contents($this->projectDir . '/apps/app.main/config/routes.override.php', '<?php');
+        file_put_contents($this->projectDir . '/apps/app.main/config/routes.php', '<?php');
+
+        $kernel = new TestKernel($this->projectDir, 'app.main', 'dev');
+
+        $imports = [];
+        $routesConfigurator = $this->createRoutingConfigurator($imports, $kernel->getEnvironment());
+        $kernel->exposeConfigureRoutes($routesConfigurator);
+
+        $expected = [
+            $this->projectDir . '/infra/config/routes/alpha.php',
+            $this->projectDir . '/infra/config/routes/dev/gamma.php',
+            $this->projectDir . '/apps/app.main/config/{routes}/*.php',
+            $this->projectDir . '/apps/app.main/config/{routes}/dev/*.php',
+            $this->projectDir . '/apps/app.main/config/routes.php',
+        ];
+
+        sort($imports);
+        sort($expected);
+
+        self::assertSame($expected, $imports);
+    }
 
     public function testDirectoryHelpersRespectServerOverrides(): void
     {
@@ -69,6 +148,21 @@ final class AbstractKernelTest extends TestCase
     /**
      * @throws ReflectionException
      */
+    public function testGetKernelParametersIncludesBundlesDefinition(): void
+    {
+        file_put_contents($this->projectDir . '/infra/config/bundles.php', '<?php return ["Infra\\\\Bundle" => ["all" => true]];');
+
+        $kernel = new TestKernel($this->projectDir, 'app.main', 'prod');
+
+        $parameters = new ReflectionMethod(AbstractKernel::class, 'getKernelParameters')->invoke($kernel);
+
+        self::assertSame($this->projectDir . '/infra/config', $parameters['.kernel.config_dir']);
+        self::assertArrayHasKey('Infra\\Bundle', $parameters['.kernel.bundles_definition']);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
     public function testListInfrastructureFilesWithOverrideSkipsOverriddenFiles(): void
     {
         mkdir($this->projectDir . '/infra/config/routes', 0o777, true);
@@ -81,12 +175,59 @@ final class AbstractKernelTest extends TestCase
         $kernel = new TestKernel($this->projectDir, 'app.main', 'dev');
 
         $method = new ReflectionMethod(AbstractKernel::class, 'listInfrastructureFilesWithOverride');
-        $method->setAccessible(true);
 
         $files = $method->invoke($kernel, $this->projectDir . '/infra/config/routes', $this->projectDir . '/apps/app.main/config/routes');
 
         self::assertCount(1, $files);
         self::assertStringContainsString('a.php', $files[0]);
+    }
+
+    public function testLoadAppEnvironmentFilesRespectsPrecedence(): void
+    {
+        $infraDir = $this->projectDir . '/infra';
+        $appDir = $this->projectDir . '/apps/app.main';
+
+        file_put_contents($infraDir . '/.env', 'FOO=infra');
+        file_put_contents($infraDir . '/.env.local', 'FOO=infra_local');
+        file_put_contents($infraDir . '/.env.dev', 'FOO=infra_dev');
+        file_put_contents($infraDir . '/.env.dev.local', 'FOO=infra_dev_local');
+        file_put_contents($appDir . '/.env', 'FOO=app');
+        file_put_contents($appDir . '/.env.local', 'FOO=app_local');
+        file_put_contents($appDir . '/.env.dev', 'FOO=app_dev');
+        file_put_contents($appDir . '/.env.dev.local', 'FOO=app_dev_local');
+
+        unset($_ENV['FOO'], $_SERVER['FOO']);
+
+        new TestKernel($this->projectDir, 'app.main', 'dev');
+
+        self::assertSame('app_dev_local', $_ENV['FOO']);
+        self::assertSame('app_dev_local', $_SERVER['FOO']);
+
+        unset($_ENV['FOO'], $_SERVER['FOO']);
+    }
+
+    public function testRegisterBundlesFiltersByEnvironment(): void
+    {
+        file_put_contents($this->projectDir . '/infra/config/bundles.php', '<?php return [
+            "Valksor\\\\Bundle\\\\Tests\\\\Kernel\\\\AllTestBundle" => ["all" => true],
+            "Valksor\\\\Bundle\\\\Tests\\\\Kernel\\\\DevTestBundle" => ["dev" => true],
+            "Valksor\\\\Bundle\\\\Tests\\\\Kernel\\\\ProdTestBundle" => ["prod" => true],
+        ];');
+
+        $kernel = new TestKernel($this->projectDir, 'app.main', 'dev');
+
+        $bundleClasses = [];
+
+        foreach ($kernel->registerBundles() as $bundle) {
+            $bundleClasses[] = $bundle::class;
+        }
+
+        sort($bundleClasses);
+
+        self::assertSame([
+            AllTestBundle::class,
+            DevTestBundle::class,
+        ], $bundleClasses);
     }
 
     protected function setUp(): void
@@ -101,6 +242,52 @@ final class AbstractKernelTest extends TestCase
     {
         $this->removeDirectory($this->projectDir);
         unset($_SERVER['APP_CACHE_DIR'], $_SERVER['APP_LOG_DIR']);
+    }
+
+    /**
+     * @param array<int, string> $imports
+     */
+    private function createContainerConfigurator(
+        array &$imports,
+    ): ContainerConfigurator {
+        $builder = new ContainerBuilder();
+        $locator = new class implements FileLocatorInterface {
+            public function locate(
+                string $name,
+                ?string $currentPath = null,
+                bool $first = true,
+            ): string {
+                return $name;
+            }
+        };
+
+        $loader = new RecordingContainerPhpFileLoader($builder, $locator, $imports);
+        $instanceof = [];
+
+        return new ContainerConfigurator($builder, $loader, $instanceof, __FILE__, 'kernel_test.php');
+    }
+
+    /**
+     * @param array<int, string> $imports
+     */
+    private function createRoutingConfigurator(
+        array &$imports,
+        string $environment,
+    ): RoutingConfigurator {
+        $collection = new RouteCollection();
+        $locator = new class implements FileLocatorInterface {
+            public function locate(
+                string $name,
+                ?string $currentPath = null,
+                bool $first = true,
+            ): string {
+                return $name;
+            }
+        };
+
+        $loader = new RecordingRoutingPhpFileLoader($locator, $imports);
+
+        return new RoutingConfigurator($collection, $loader, __FILE__, __FILE__, $environment);
     }
 
     private function removeDirectory(
@@ -131,6 +318,71 @@ final class AbstractKernelTest extends TestCase
 /**
  * @internal
  */
+final class RecordingContainerPhpFileLoader extends DiPhpFileLoader
+{
+    /** @var array<int, string> */
+    private array $imports;
+
+    /**
+     * @param array<int, string> $imports
+     */
+    public function __construct(
+        ContainerBuilder $container,
+        FileLocatorInterface $locator,
+        array &$imports,
+    ) {
+        parent::__construct($container, $locator);
+        $this->imports = &$imports;
+    }
+
+    public function import(
+        mixed $resource,
+        ?string $type = null,
+        string|bool $ignoreErrors = false,
+        ?string $sourceResource = null,
+        mixed $exclude = null,
+    ): mixed {
+        $this->imports[] = (string) $resource;
+
+        return null;
+    }
+}
+
+/**
+ * @internal
+ */
+final class RecordingRoutingPhpFileLoader extends RoutingPhpFileLoader
+{
+    /** @var array<int, string> */
+    private array $imports;
+
+    /**
+     * @param array<int, string> $imports
+     */
+    public function __construct(
+        FileLocatorInterface $locator,
+        array &$imports,
+    ) {
+        parent::__construct($locator);
+        $this->imports = &$imports;
+    }
+
+    public function import(
+        mixed $resource,
+        ?string $type = null,
+        string|bool $ignoreErrors = false,
+        ?string $sourceResource = null,
+        mixed $exclude = null,
+    ): array {
+        $this->imports[] = (string) $resource;
+
+        return [];
+    }
+}
+
+/**
+ * @internal
+ */
 final class TestKernel extends AbstractKernel
 {
     protected ?string $apps = 'apps';
@@ -144,8 +396,32 @@ final class TestKernel extends AbstractKernel
         parent::__construct($environment, true, $id);
     }
 
+    public function exposeConfigureContainer(
+        ContainerConfigurator $configurator,
+    ): void {
+        $this->configureContainer($configurator);
+    }
+
+    public function exposeConfigureRoutes(
+        RoutingConfigurator $routes,
+    ): void {
+        $this->configureRoutes($routes);
+    }
+
     public function getProjectDir(): string
     {
         return $this->projectDirOverride;
     }
+}
+
+final class AllTestBundle extends Bundle
+{
+}
+
+final class DevTestBundle extends Bundle
+{
+}
+
+final class ProdTestBundle extends Bundle
+{
 }
